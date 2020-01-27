d5224d5935c4b0b4b19958ff171c8026d70240bb
Rohit Chopra <crohit92@gmail.com>
2020-01-28 00:00:09 +0530

    Implement Hooks in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component } from "@angular/core";
-import { LoggerService } from "./core/logger/logger.service";
+import { Component } from '@angular/core';
+import { LoggerService } from './core/logger/logger.service';
 import { Hooks } from './shared/interfaces/hooks.interface';
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements Hooks {
-  constructor(private readonly logger: LoggerService) {
-    logger.log("ctor: App Component");
-  }
-  name = "Angular";
-  onInit() {}
+    ngOnChanges() {
+        this.logger.log('ngOnChanges: AppComponent');
+    }
+    ngOnInit(): void {
+        this.logger.log('ngOnInit: AppComponent');
+    }
+    ngDoCheck(): void {
+        this.logger.log('ngDoCheck: AppComponent');
+    }
+    ngAfterContentInit(): void {
+        this.logger.log('ngAfterContentInit: AppComponent');
+    }
+    ngAfterContentChecked(): void {
+        this.logger.log('ngAfterContentChecked: AppComponent');
+    }
+    ngAfterViewInit(): void {
+        this.logger.log('ngAfterViewInit: AppComponent');
+    }
+    ngAfterViewChecked(): void {
+        this.logger.log('ngAfterViewChecked: AppComponent');
+    }
+    constructor(private readonly logger: LoggerService) {
+        logger.log('ctor: App Component');
+    }
+    name = 'Angular';
 }
